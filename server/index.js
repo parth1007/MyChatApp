114a89e2cef9f2fd38d74b7040ccd4cfdd23c55e
parth1007 <parthsoni03ps@gmail.com>
2022-06-16 00:37:33 +0530

    deployment code changed

--- server/index.js
+++ server/index.js
@@ -40,10 +40,7 @@ app.use("/api/message", messageRoutes);
   if (process.env.NODE_ENV === "production") {
   
     app.use("/", express.static(path.join(__dirname1, "../client/build")));
-    app.get("*", (req, res) =>{
-      res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-      }
-    );
+    
   } else {
     app.get("/", (req, res) => {
       res.send("API is running..");
